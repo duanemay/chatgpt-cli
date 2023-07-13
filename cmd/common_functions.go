34f643d1249d0ad05a3726b4c3f522da1b7f97b2
Duane May <duanemay@gmail.com>
2023-07-13 01:02:49 -0400

    Rework session-file handling in loadOrCreateChatCompletionRequest
    Refactor loadOrCreateChatCompletionRequest function to allow the use of non-existent sessionFile. Prior logic required the sessionFile to exist, but we now create a new instance if the provided file doesn't exist or if no sessionFile is given. This update improves flexibility and user experience of the ChatFlags functionality by providing more options for session continuity and preservation. And makes scripting with prior known file name easier.

--- cmd/common_functions.go
+++ cmd/common_functions.go
@@ -28,13 +28,35 @@ func detectTerminal() bool {
 	return false
 }
 
-// loadOrCreateChatCompletionRequest reads the sessionFile if provided, or creates a new one
-func loadOrCreateChatCompletionRequest(f *ChatFlags, chatContext *ChatContext) (chat *openai.ChatCompletionRequest) {
-	if f.sessionFile == "" {
-		f.sessionFile = "chatgpt-cli-" + time.Now().UTC().Format(time.RFC3339) + ".json"
-		if chatContext.InteractiveSession && shouldWriteSession(f) {
-			fmt.Printf("  to continue with this session, use: --%s %s\n", FlagSessionFile, f.sessionFile)
+// loadOrCreateChatCompletionRequest
+// if a sessionFile is provided, and it exists, then it is loaded
+// if a sessionFile is provided, and it does not exist, then it is created
+// if a sessionFile is not provided, then a new session and sessionFile is created
+func loadOrCreateChatCompletionRequest(f *ChatFlags, chatContext *ChatContext) *openai.ChatCompletionRequest {
+	var chat *openai.ChatCompletionRequest = nil
+
+	// if a sessionFile is provided check if it exists
+	if f.sessionFile != "" {
+		if _, err := os.Stat(f.sessionFile); err == nil {
+			// sessionFile provided and exists, load it
+			chat = loadSessionFile(f.sessionFile)
+			if chatContext.InteractiveSession {
+				fmt.Printf("  continuing session from file: %s\n", f.sessionFile)
+			}
+
+			// update the chat parameters from the flags
+			chat.Model = f.model
+			chat.Temperature = f.temperature
+			chat.MaxTokens = f.maxTokens
+			chat.TopP = f.topP
 		}
+	} else {
+		// no sessionFile provided, create a new sessionFile name
+		f.sessionFile = "chatgpt-cli-" + time.Now().UTC().Format(time.RFC3339) + ".json"
+	}
+
+	// if a sessionFile was not provided, or it did not exist, create a new session
+	if chat == nil {
 		chat = &openai.ChatCompletionRequest{
 			Model:       f.model,
 			Messages:    []openai.ChatCompletionMessage{},
@@ -42,18 +64,12 @@ func loadOrCreateChatCompletionRequest(f *ChatFlags, chatContext *ChatContext) (
 			MaxTokens:   f.maxTokens,
 			TopP:        f.topP,
 		}
-	} else {
-		chat = loadSessionFile(f.sessionFile)
-		chat.Model = f.model
-		chat.Temperature = f.temperature
-		chat.MaxTokens = f.maxTokens
-		chat.TopP = f.topP
-
-		if chatContext.InteractiveSession {
-			fmt.Printf("  continuing session from file: %s\n", f.sessionFile)
+		if chatContext.InteractiveSession && shouldWriteSession(f) {
+			fmt.Printf("  to continue with this session, use: --%s %s\n", FlagSessionFile, f.sessionFile)
 		}
 	}
-	return
+
+	return chat
 }
 
 // shouldWriteSession determines if the sessionFile should be written to disk
