c56c97cc2dedc6e0cc21aca3557a34b0182492e0
Duane May <duanemay@gmail.com>
2025-02-13 22:01:36 -0500

    Update demo gifs

--- docs/generate-demos.sh
+++ docs/generate-demos.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2086 # allow globing
+
 set -e
 
 filter=${1:-*}
 
 cd docs || exit
 echo "Using filter=${filter}"
-ls -l "${filter}-demo.tape";
+ls -l ${filter}-demo.tape;
 
 rm chatgpt-cli*.json dall-e-*.png || true
 export  PS1='\[\033[31;1m\]\$\[\033[m\] '
