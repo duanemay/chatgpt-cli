9ef799b539775d21727a8f096dbdd1d332088f53
Duane May <duanemay@gmail.com>
2025-07-13 13:20:42 -0400

    Update demo gifs

--- docs/generate-demos.sh
+++ docs/generate-demos.sh
@@ -9,12 +9,12 @@ cd docs || exit
 echo "Using filter=${filter}"
 ls -l ${filter}-demo.tape;
 
-rm chatgpt-cli*.json dall-e-*.png || true
+rm chatgpt-cli*.json dall-e-*.png image-*.png || true
 export  PS1='\[\033[31;1m\]\$\[\033[m\] '
 
 for file in ${filter}-demo.tape; do
   echo "Running ${file}"
   vhs "${file}"
-  rm chatgpt-cli*.json dall-e-*.png || true
+  rm chatgpt-cli*.json dall-e-*.png image-*.png || true
   echo
 done
