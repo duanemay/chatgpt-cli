83d28443f03bb1d0de15ae30d95568aa820dca39
Duane May <duanemay@gmail.com>
2025-09-14 12:54:53 -0400

    Update default model to GPT-5

--- cmd/args.go
+++ cmd/args.go
@@ -32,7 +32,7 @@ const (
 
 const (
 	defaultMaxTokens     = 0
-	defaultModel         = openai.GPT4oMini
+	defaultModel         = openai.GPT5
 	defaultRole          = openai.ChatMessageRoleUser
 	defaultTemperature   = 1.0
 	defaultTopP          = 1.0
